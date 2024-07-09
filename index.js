18948cafdd00221c3879dcd38f0b6b5238e86323
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-10 01:37:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(toDo = "roller-skate") {
+    return `This Saturday, I want to ${toDo}!`
+}
+saturdayFun();
+
+
+function  mondayWork(toDo = 'go to the office') {
+    return `This Monday, I will ${toDo}.`
+}
+mondayWork();
+
+
+function wrapAdjective(flair = '*') {
+    return function (text = 'a dedicated programmer') {
+        return `You are ${flair}${text}${flair}!`;
+    }
+}
